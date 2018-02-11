42cea218f46bf56864934d59733fa60322a8514c
Christoph Lenovo <christophhedtrich@gmail.com>
2018-02-11 17:50:25 +0100

    added golden section search

--- src/ValueFunctionSolver.jl
+++ src/ValueFunctionSolver.jl
@@ -1,5 +1,59 @@
 module ValueFunctionSolver
 
-# package code goes here
+    export gss
+    const invphi = (sqrt(5) - 1) / 2
+    const invphi2 = (3 - sqrt(5)) / 2
+    """
+    Golden section search.
+
+    Given a function f with a single local minumum in
+    the interval [a,b], gss returns a subset interval
+    [c,d] that contains the minimum with d-c <= tol.
+
+    example:
+    f(x) = (x-2)^2
+    a = 1
+    b = 5
+    tol = 1e-5
+    bracket = gss(f, a, b, tol = tol)
+    result:
+    (1.9999959837979107, 2.0000050911830893)
+    """
+    function gss(f,a::Float64,b::Float64; tol::Float64=1e-5)
+        (a,b)=(min(a,b),max(a,b))
+        h = b - a
+        if h <= tol
+            return (a,b)
+        end
+        # required steps to achieve tolerance
+        n = Int(ceil(log(tol/h)/log(invphi)))
+        c = a + invphi2 * h
+        d = a + invphi * h
+        yc = f(c)
+        yd = f(d)
+
+        for k in 1:n
+            if yc < yd
+                b = d
+                d = c
+                yd = yc
+                h = invphi*h
+                c = a + invphi2 * h
+                yc = f(c)
+            else
+                a = c
+                c = d
+                yc = yd
+                h = invphi*h
+                d = a + invphi * h
+                yd = f(d)
+            end
+        end
+        if yc < yd
+            return (a,d)
+        else
+            return (c,b)
+        end
+    end
 
 end # module
